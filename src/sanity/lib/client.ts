83ca403b3d66ca03bc259b8daab50d779c623e72
albertmanuel <albertmanuels10@gmail.com>
2025-08-05 00:05:23 +0700

    feat: get api_version from .env on sanity client

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: '2024-01-01',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: process.env.NODE_ENV === 'production',
 })
